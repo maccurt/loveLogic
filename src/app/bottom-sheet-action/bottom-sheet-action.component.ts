fbbaf3c04f2d5738358ae9328f38d3d74ce6f500
Michael McGee <dineromike@yahoo.com>
2024-12-29 09:36:26 -0600

    removed import for use

--- src/app/bottom-sheet-action/bottom-sheet-action.component.ts
+++ src/app/bottom-sheet-action/bottom-sheet-action.component.ts
@@ -3,12 +3,10 @@ import { BusinessStore } from '../business-domain/business.store';
 import { BusinessDetailComponent } from "../business-domain/business-detail/business-detail.component";
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { CdkCopyToClipboard } from '@angular/cdk/clipboard';
-import { BusinessComponent } from "../business-domain/business/business.component";
 
 @Component({
   selector: 'll-bottom-sheet-action',
-  imports: [BusinessDetailComponent, MatButtonModule, MatIconModule, CdkCopyToClipboard, BusinessComponent],
+  imports: [BusinessDetailComponent, MatButtonModule, MatIconModule],
   providers: [],
   templateUrl: './bottom-sheet-action.component.html',
   styleUrl: './bottom-sheet-action.component.scss'
